657c83c5a397c9274e750303a1cc39c37b807bc9
Ruslan Yun <leonemsolis@gmail.com>
2020-03-09 00:51:12 +0900

    Changed gravity logic

---
+++ Assets/Scripts/CarControllerPath.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarControllerPath : MonoBehaviour
+{
+    Transform earth;
+    bool grounded = false;
+
+    void Start()
+    {
+        earth = FindObjectOfType<EarthController>().transform;
+        StartCoroutine(StartMovement());
+    }
+
+    IEnumerator StartMovement() {
+        yield return new WaitForSeconds(2f);
+        grounded = true;
+    }
+
+    void FixedUpdate()
+    {
+        // Gravity
+        RaycastHit hit;
+        if(Physics.Raycast(transform.position, earth.position - transform.position, out hit)) {
+            Debug.DrawRay(transform.position, (earth.position - transform.position));
+            float lastY = transform.localRotation.eulerAngles.y;
+            transform.position = hit.point;
+            transform.up = transform.position.normalized;
+        }
+        // Movement
+        if(grounded) {
+            transform.position += 30f * Time.deltaTime * transform.TransformDirection(Vector3.forward);
+        }
+    }
+}
